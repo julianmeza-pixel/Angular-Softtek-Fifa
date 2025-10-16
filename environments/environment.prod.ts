6d77da7cd2981fbb67c5ccf9aad8aa89916c31ea
Jeisson Andres Patiño Ramirez <45497602+jeissonPatino@users.noreply.github.com>
2025-10-16 16:23:10 -0500

    feat/Rolback

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend-en-produccion.com/api/candidates'
+  apiUrl: 'https://tu-backend-en-produccion.com/api/candidates',
+  apiUrlRoles: 'https://seniorityfinder-b2e5bjfraff9bbaw.canadacentral-01.azurewebsites.net/api/roles',
+  apiUrlSeniority: 'https://seniorityfinder-b2e5bjfraff9bbaw.canadacentral-01.azurewebsites.net/api/seniorities',
+  apiUrlTechno:  'https://seniorityfinder-b2e5bjfraff9bbaw.canadacentral-01.azurewebsites.net/api/technologies',
+  defaultUserImage: '/assets/images/default-user.png',
+  defaultUserRoleText: 'sin rol encontrado'
 };
